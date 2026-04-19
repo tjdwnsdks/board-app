bd67e94d983f31579e5fe30d5fc99c5c7187eef6
junjun <tjdwnsdks@naver.com>
2026-04-19 23:49:09 +0900

    .

--- mysql/init.sql
+++ mysql/init.sql
@@ -1,3 +1,8 @@
+SET NAMES utf8mb4;
+SET character_set_client = utf8mb4;
+SET character_set_connection = utf8mb4;
+SET character_set_results = utf8mb4;
+
 CREATE DATABASE IF NOT EXISTS boarddb CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 USE boarddb;
 
